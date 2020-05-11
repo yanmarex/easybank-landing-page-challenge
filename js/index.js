02695bb33254548db6d971e16c131856151c70fe
yanmarex <60897746+yanmarex@users.noreply.github.com>
2020-05-11 17:31:16 -0400

    Add change event icon in navbar

--- js/index.js
+++ js/index.js
@@ -3,4 +3,5 @@ const navbar = document.querySelector("#navbar");
 
 btn.addEventListener("click", function() {
     navbar.classList.toggle('is-hidden');
+    btn.classList.toggle('btn-menu-close');
 });
